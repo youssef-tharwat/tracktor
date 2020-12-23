165f6be438812f280c7a637d8d436fc2afa70059
Youssef <yt@papershift.com>
2020-12-23 20:26:26 +0100

    Removed vue router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from "vue";
 import App from "./popup/App";
-import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   store,
   render: h => h(App)
 }).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,13 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    steps: [
+      {
+        name: 'selection',
+      }
+    ]
+  },
   mutations: {},
   actions: {},
   modules: {}
